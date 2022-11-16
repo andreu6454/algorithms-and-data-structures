8ac19c04125e7225b9ac596314766debbe24af21
Andrey Malin <andreu6454@gmail.com>
2022-11-16 23:23:59 +0300

    stack

--- stack/stack.js
+++ stack/stack.js
@@ -1,23 +1,29 @@
 class Stack {
     constructor() {
         this.stack = [];
+        this.head = 0;
     }
-
     push(el) {
-        this.stack.push(el);
+        this.stack[this.head] = el;
+        this.head++
     }
-
     pop() {
-        if(this.stack[0]){
-           return  this.stack.pop();
+        if (this.stack[0]) {
+            const item = this.stack[this.head - 1];
+            delete this.stack[this.head - 1];
+            this.head--;
+            return item;
         } else {
             return "Stack clear";
         }
     }
-    length() {
-        return this.stack.length;
+    get peek(){
+        return this.stack[this.head - 1];
     }
-    isEmpty() {
+    get length() {
+        return this.head;
+    }
+    get isEmpty() {
         return this.stack.length === 0;
     }
 }
@@ -29,16 +35,11 @@ stack.push(4);
 stack.push(5);
 stack.push(6);
 
-console.log(stack.length()) // ==> 4
-
-console.log(stack.isEmpty())  // ==> false
-
+console.log(stack.length) // ==> 4
+console.log(stack.isEmpty)  // ==> false
+console.log(stack.peek)  // ==> 6
 console.log(stack.pop())  // ==> 6
-
 console.log(stack.pop())  // ==> 5
-
 console.log(stack.pop())  // ==> 4
-
 console.log(stack.pop())  // ==> 2
-
 console.log(stack.pop())  // ==> Stack clear
